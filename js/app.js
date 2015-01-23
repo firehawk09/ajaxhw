3632f7c09cef3a417292b795c18564216d8e3560
Rafael.Quijada <ralphybook@gmail.com>
2015-01-23 09:59:22 -0600

    morning update

--- js/app.js
+++ js/app.js
@@ -15,8 +15,13 @@ function app() {
             url: "./bower_components/jquery/dist/jquery.min.js"
         }, {
             url: "./bower_components/lodash/dist/lodash.min.js"
+        }, {
+        //     url: "./js/github.js"
+        // },
+
+            url: "./bower_components/backbone/backbone.js"
         }
-        // {url: "./bower_components/backbone/backbone.js"}
+
     ).then(function() {
         document.querySelector("html").style.opacity = 1;
         // start app?
@@ -24,56 +29,52 @@ function app() {
         //debugger
 
 
-        var token = "c95ae7b1e65037f31fa13bfa3b4893400bf92c5c"
-
-        var githubUsername = "firehawk09";
-        var url = "https://api.github.com/users/" + githubUsername + "?access_token=" + token;
-
-        $.get(url).then(drawProfile);
-
-        function drawProfile(data) {
-            document.getElementsByClassName("leftSide")[0].innerHTML = [
-                '<img src="https://avatars.githubusercontent.com/u/10188453?v=3">',
-                '<h2>',
-                data.name,
-                '</h2>',
-                '<h4> Email: ',
-                data.email,
-                '</h4>',
-                '<h4> Public Repos: ',
-                data.public_repos,
-                '</h4>',
-                '<h4> Location: ',
-                data.location,
-                '</h4>',
-                arguments[1]
-            ].join('')
-        }
-
-//         var url2 = "https://api.github.com/users/" + githubUsername+ "?access_token=" + token + "/repos/";
-
-//         $.get(url2).then(drawProfile2);
-
-// // ====================================
-
-//         (myRepos.then(function(data)
-//         {drawRepos(data)};
 
 
-//         var myObject = $.get("something");
+        // githubClient(firehawk09, c95ae7b1e65037f31fa13bfa3b4893400bf92c5c)
 
-//         myObject.then(
-//         return Unacceptableeeee!!!!);
+        //<------------------------------------------------------------------->
 
-// // ====================================
 
-//         function drawProfile2(data) {
-//             document.getElementsByClassName("rightSide")[0].innerHTML = [
-//                 '<h4> Repos:',
-//                 data.repos_url,
-//                 '</h4>',
-//                 arguments[1]
-//             ].join('')
-//         }
+
+
+
+
+            var token = "c95ae7b1e65037f31fa13bfa3b4893400bf92c5c"
+            var githubUsername = "firehawk09";
+
+            var profile_url = "https://api.github.com/users/" + githubUsername + "?access_token=" + token;
+            var left_template_url = "./templates/left.html";
+            var right_template_url = "./templates/right.html";
+            var repos_url = "https://api.github.com/users/" + githubUsername + "/repos?access_token=" + token;
+
+
+            var promise1 = $.get(profile_url);
+            var promise2 = $.get(left_template_url);
+
+            $.when(promise1, promise2).then(drawProfile)
+
+
+        function drawProfile() {
+            var data = arguments[0]
+            var template = arguments[1]
+            document.getElementsByClassName("left")[0].innerHTML += _.template(template[0], data[0]);
+            }
+
+
+            var promise3 = $.get(repos_url);
+            var promise4 = $.get(right_template_url);
+
+            $.when(promise3, promise4).then(drawRepos)
+
+
+            function drawRepos() {
+
+        var repo = arguments[0][0]
+        var template = arguments[1]
+        repo.forEach(function(value) {
+            document.getElementsByClassName("right")[0].innerHTML += _.template(template[0], value);
+                })
+            }
     })
 }
